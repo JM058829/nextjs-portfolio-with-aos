e85a6afa59caef2f38cd6ec8a580fd1c77dfd3c7
JM058829 <jm058829@gmail.com>
2025-06-10 18:52:48 -0700

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -128,7 +128,7 @@ export const servicesItems = [
 
 export const testimonialsItems = [
   {
-    name: "Santa",
+    name: "Tony",
     review:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: Avatar1,
